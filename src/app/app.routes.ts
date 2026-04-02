cd97954786e595b898b5b1b6f58e6b5e34ec0ab9
Jocelyn <jocelynloisy500@gmail.com>
2026-04-02 11:14:49 +0200

    feat(pages): add About and Contact pages with minimalistic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router'
 import { Home } from './features/home/home'
+import {About} from './features/about/about';
+import {Contact} from './features/contact/contact';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
 ];
